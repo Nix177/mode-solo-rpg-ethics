9673dba3a8acb8b829b692e299eb689df79b5107
tuorn <nicolas.tuor@unifr.ch>
2026-05-19 08:53:02 +0200

    Tune XP bar transparency

--- action_rpg_mode/game.js
+++ action_rpg_mode/game.js
@@ -2,7 +2,7 @@
 const TILE_SIZE = 48;
 const DEFAULT_WORLD_TILES = { w: 92, h: 58 };
 const MAX_ENEMIES = 120;
-const ASSET_VERSION = "20260519_minemusic1";
+const ASSET_VERSION = "20260519_xpalpha1";
 
 const RESOURCE_RULES = {
   runStaminaPerSecond: 24,
